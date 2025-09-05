475c3bea221fcbc55d3d223a91a0416b1e324c42
Davide Pedron <davide@pedron.dev>
2025-09-05 02:07:07 +0200

    Initial commit with the first page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://pedron.dev",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
